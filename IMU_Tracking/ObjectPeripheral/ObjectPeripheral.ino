16090263b73f3aa0447293f803f7d93e2b05354d
Gal Gorjup <ggor290@aucklanduni.ac.nz>
2020-05-26 14:44:40 +1200

    Implement transmission of a float characteristic value

---
+++ IMU_Tracking/ObjectPeripheral/ObjectPeripheral.ino
@@ -0,0 +1,89 @@
+#include <ArduinoBLE.h>
+
+// BLE position service
+BLEService pose_service("180F");
+
+// BLE x y z characteristics
+BLEFloatCharacteristic x_characteristic("2A10", BLERead);
+BLEFloatCharacteristic y_characteristic("2A11", BLERead);
+BLEFloatCharacteristic z_characteristic("2A12", BLERead);
+
+// Object pose components
+float x = 8.69;
+float y = 0;
+float z = 0;
+
+long previousMillis = 0;
+
+void setup() {
+  Serial.begin(9600);    // initialize serial communication
+  while (!Serial);
+
+  pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
+
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
+
+  /* Set a local name for the BLE device
+     This name will appear in advertising packets
+     and can be used by remote devices to identify this BLE device
+     The name can be changed but maybe be truncated based on space left in advertisement packet
+  */
+  BLE.setLocalName("SensorizedObject");
+  // Add the service UUID
+  BLE.setAdvertisedService(pose_service); 
+  // Add characteristics
+  pose_service.addCharacteristic(x_characteristic); 
+  // Add services
+  BLE.addService(pose_service); 
+  // Set initial value for characteristics
+  x_characteristic.writeValue(x); 
+  
+  /* Start advertising BLE.  It will start continuously transmitting BLE
+     advertising packets and will be visible to remote BLE central devices
+     until it receives a new connection */
+  // Start advertising
+  BLE.advertise();
+
+  Serial.println("Bluetooth device active, waiting for connections...");
+}
+
+void loop() {
+  // Wait for a BLE central
+  BLEDevice central = BLE.central();
+
+  // If a central is connected to the peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // Print the central's BT address:
+    Serial.println(central.address());
+    // Turn on the LED to indicate the connection:
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // While the central is connected, update object pose
+    while (central.connected()) {
+      long currentMillis = millis();
+      if (currentMillis - previousMillis >= 100) {
+        previousMillis = currentMillis;
+        update_position();
+      }
+    }
+    // When the central disconnects, turn off the LED:
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
+}
+
+void update_position() {
+  x_characteristic.writeValue(x);
+  byte x_byte[4];
+  x_characteristic.readValue(x_byte, 4);
+  Serial.println(x_byte[0]);
+  Serial.println(x_byte[1]);
+  Serial.println(x_byte[2]);
+  Serial.println(x_byte[3]);
+}
---
+++ IMU_Tracking/PCCentral/PCCentral.ino
@@ -0,0 +1,130 @@
+#include <ArduinoBLE.h>
+
+long previousMillis = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  // Initialize the BLE hardware
+  BLE.begin();
+
+  Serial.println("BLE Central - Sensorized Object");
+
+  // Start scanning for peripherals
+  BLE.scan();
+}
+
+void loop() {
+  // Check if a peripheral has been discovered
+  BLEDevice peripheral = BLE.available();
+
+  if (peripheral) {
+    // Discovered a peripheral, print out address, local name, and advertised service
+    Serial.print("Found ");
+    Serial.print(peripheral.address());
+    Serial.print(" '");
+    Serial.print(peripheral.localName());
+    Serial.print("' ");
+    Serial.print(peripheral.advertisedServiceUuid());
+    Serial.println();
+
+    if (peripheral.localName() != "SensorizedObject") {
+      return;
+    }
+
+    // stop scanning
+    BLE.stopScan();
+
+    read_pose(peripheral);
+
+    // Peripheral disconnected, start scanning again
+    BLE.scan();
+  }
+}
+
+void read_pose(BLEDevice peripheral) {
+  // Connect to the peripheral
+  Serial.println("Connecting ...");
+
+  if (peripheral.connect()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Failed to connect!");
+    return;
+  }
+
+  // Discover peripheral attributes
+  Serial.println("Discovering attributes ...");
+  if (peripheral.discoverAttributes()) {
+    Serial.println("Attributes discovered");
+  } else {
+    Serial.println("Attribute discovery failed!");
+    peripheral.disconnect();
+    return;
+  }
+
+  // Retrieve the position characteristics
+  BLECharacteristic x_characteristic = peripheral.characteristic("2A10");
+
+  if (!x_characteristic) {
+    Serial.println("Peripheral does not have position characteristic");
+    peripheral.disconnect();
+    return;
+  }
+
+  // While the peripheral is connected
+  while (peripheral.connected()) {
+    long currentMillis = millis();
+    if (currentMillis - previousMillis >= 100) {
+      previousMillis = currentMillis;
+
+      //x_characteristic.read();
+      //Serial.println(x_characteristic.valueSize());
+      
+      
+      // Read pose
+      //x_characteristic.read();
+      // Serial.print(", value ");
+      // printData(x_characteristic.value(), x_characteristic.valueLength());
+      //byte xb = x_characteristic.value();
+      //Serial.println(x);
+      
+      byte x_byte[4];
+      x_characteristic.readValue(x_byte, 4);
+      float x = byte_to_float(x_byte);
+      Serial.println(x);
+      
+    }
+  }
+
+  Serial.println("Peripheral disconnected");
+}
+
+void printData(const unsigned char data[], int length) {
+  for (int i = 0; i < length; i++) {
+    unsigned char b = data[i];
+
+    if (b < 16) {
+      Serial.print("0");
+    }
+
+    Serial.print(b, HEX);
+  }
+}
+
+
+union byte2float {
+  byte b[4];
+  float fval;
+};
+
+float byte_to_float(const byte data[4]) {
+  byte2float converter;
+  converter.b[0] = data[0];
+  converter.b[1] = data[1];
+  converter.b[2] = data[2];
+  converter.b[3] = data[3];
+  
+  return converter.fval;
+}
