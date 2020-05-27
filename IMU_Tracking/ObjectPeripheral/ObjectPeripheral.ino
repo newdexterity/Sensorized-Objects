451d30bcf69682bece7b873b3cc6f030a9c7751b
Gal Gorjup <ggor290@aucklanduni.ac.nz>
2020-05-27 19:10:20 +1200

    Include Madgwick AHRS

--- IMU_Tracking/ObjectPeripheral/ObjectPeripheral.ino
+++ IMU_Tracking/ObjectPeripheral/ObjectPeripheral.ino
@@ -1,4 +1,6 @@
 #include <ArduinoBLE.h>
+#include <Arduino_LSM6DS3.h>
+#include <MadgwickAHRS.h>
 
 // BLE position service
 BLEService pose_service("180F");
@@ -13,15 +15,41 @@ float x = 8.69;
 float y = 0;
 float z = 0;
 
+// BLE loop variables
 long previousMillis = 0;
 
+// AHRS
+Madgwick filter;
+unsigned long microsPerReading, microsPrevious;
+float accelScale, gyroScale;
+float gx_add=0, gy_add=0, gz_add=0;
+
 void setup() {
-  Serial.begin(9600);    // initialize serial communication
+  // Initialise serial communication
+  Serial.begin(9600);    
   while (!Serial);
 
-  pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
+  // Start IMU
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
 
-  // begin initialization
+  // Start filter with 104 Hz, which is the sampling frequency of the IMU
+  filter.begin(104);
+
+  // Initialize variables to pace updates to correct rate
+  microsPerReading = 1000000 / 104;
+  microsPrevious = micros();
+
+  // Calibrate gyro
+  delay(500);
+  calibrateGyroOffset(3000);
+
+  // Initialise the built-in LED pin to indicate when a central is connected
+  pinMode(LED_BUILTIN, OUTPUT); 
+
+  // Begin BLE
   if (!BLE.begin()) {
     Serial.println("starting BLE failed!");
     while (1);
@@ -52,6 +80,39 @@ void setup() {
 }
 
 void loop() {
+  // AHRS loop variables
+  float ax, ay, az;
+  float gx, gy, gz;
+  float roll, pitch, heading;
+  unsigned long microsNow;
+
+  // check if it's time to read data and update the filter
+  microsNow = micros();
+  if (microsNow - microsPrevious >= microsPerReading) {
+
+    // Read acceleration and gyro
+    IMU.readAcceleration(ax, ay, az);
+    IMU.readGyroscope(gx, gy, gz);
+
+    // Update the filter, which computes orientation
+    filter.updateIMU(gx-gx_add, gy-gy_add, gz-gz_add, ax, ay, az);
+
+    // Print the heading, pitch and roll
+    roll = filter.getRoll();
+    pitch = filter.getPitch();
+    heading = filter.getYaw();
+    Serial.print("Orientation: ");
+    Serial.print(heading);
+    Serial.print(" ");
+    Serial.print(pitch);
+    Serial.print(" ");
+    Serial.println(roll);
+
+    // Increment previous time, so we keep proper pace
+    microsPrevious = microsPrevious + microsPerReading;
+  }
+  return;
+  
   // Wait for a BLE central
   BLEDevice central = BLE.central();
 
@@ -63,13 +124,48 @@ void loop() {
     // Turn on the LED to indicate the connection:
     digitalWrite(LED_BUILTIN, HIGH);
 
+    // Update previous micros
+    microsPrevious = micros();
+
     // While the central is connected, update object pose
     while (central.connected()) {
-      long currentMillis = millis();
-      if (currentMillis - previousMillis >= 100) {
-        previousMillis = currentMillis;
-        update_position();
+      // AHRS loop variables
+      float ax, ay, az;
+      float gx, gy, gz;
+      float roll, pitch, heading;
+      unsigned long microsNow;
+
+      // check if it's time to read data and update the filter
+      microsNow = micros();
+      if (microsNow - microsPrevious >= microsPerReading) {
+    
+        // Read acceleration and gyro
+        IMU.readAcceleration(ax, ay, az);
+        IMU.readGyroscope(gx, gy, gz);
+    
+        // Update the filter, which computes orientation
+        filter.updateIMU(gx, gy, gz, ax, ay, az);
+    
+        // Print the heading, pitch and roll
+        roll = filter.getRoll();
+        pitch = filter.getPitch();
+        heading = filter.getYaw();
+        Serial.print("Orientation: ");
+        Serial.print(heading);
+        Serial.print(" ");
+        Serial.print(pitch);
+        Serial.print(" ");
+        Serial.println(roll);
+    
+        // Increment previous time, so we keep proper pace
+        microsPrevious = microsPrevious + microsPerReading;
       }
+      
+      //long currentMillis = millis();
+      //if (currentMillis - previousMillis >= 100) {
+      //  previousMillis = currentMillis;
+      //  update_position();
+      //}
     }
     // When the central disconnects, turn off the LED:
     digitalWrite(LED_BUILTIN, LOW);
@@ -77,6 +173,30 @@ void loop() {
     Serial.println(central.address());
   }
 }
+
+// Don't move the board during calibration
+void calibrateGyroOffset(unsigned int duration_ms){  
+  unsigned long count = 0, start = millis();
+  float gx, gy, gz, addX=0, addY=0, addZ=0;
+  while ((millis()-start) < duration_ms){ 
+    if (IMU.gyroscopeAvailable()){  
+      IMU.readGyroscope(gx, gy, gz); 
+      count++;
+      addX += gx; addY += gy; addZ += gz;
+      digitalWrite(LED_BUILTIN, (millis()/125)%2);       // blink onboard led every 250ms
+    }
+  }
+  // Store the average measurements as offset
+  gx_add = addX/count;
+  gy_add = addY/count;
+  gz_add = addZ/count;
+  // Onboard led off
+  digitalWrite(LED_BUILTIN, 0);
+  Serial.println("Calibrated gyro with nr of samples: "+String(count));
+  Serial.println(gx_add);
+  Serial.println(gy_add);
+  Serial.println(gz_add);
+}   
 
 void update_position() {
   x_characteristic.writeValue(x);
